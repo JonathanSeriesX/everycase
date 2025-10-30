d6d94edadcd4e81c2ceb87fcf40ca6df87ab71a1
JonathanSeriesX <12529409+JonathanSeriesX@users.noreply.github.com>
2025-10-30 17:57:47 +0000

    Next.js 16 update

---
+++ next.config.ts
@@ -0,0 +1,29 @@
+import type { NextConfig } from "next";
+import nextra from "nextra";
+
+const withNextra = nextra({
+  whiteListTagsStyling: ["h1"],
+});
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "everycase.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cloudfront.everycase.org",
+        pathname: "/**",
+      },
+    ],
+    unoptimized: true,
+    //quality: 100,
+    formats: ["image/avif", "image/webp"],
+    imageSizes: [512, 1536],
+  },
+};
+
+export default withNextra(nextConfig);
