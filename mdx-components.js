afd3c9cc790740494e577703989c622e6b6d20a1
JonathanSeriesX <12529409+JonathanSeriesX@users.noreply.github.com>
2025-11-01 23:49:13 +0000

    More polish

--- mdx-components.js
+++ mdx-components.js
@@ -1,4 +1,5 @@
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs"; // nextra-theme-blog or your custom theme
+import { LinkArrowIcon } from "nextra/icons"; // same icon they use
 
 // Get the default MDX components
 const themeComponents = getThemeComponents();
@@ -7,6 +8,23 @@ const themeComponents = getThemeComponents();
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
+    // Beautiful arrow after links
+    a({ href = "", children, ...props }) {
+      const showArrow = href && !href.startsWith("#");
+      return (
+        <themeComponents.a href={href} {...props}>
+          {children}
+          {showArrow && (
+            <>
+              <LinkArrowIcon
+                className="inline align-baseline shrink-0"
+                height="1em"
+              />
+            </>
+          )}
+        </themeComponents.a>
+      );
+    },
     ...components,
   };
 }
