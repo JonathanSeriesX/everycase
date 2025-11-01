ac1993cd6d5f1efdd2b3a9a616463d6bf2282333
JonathanSeriesX <12529409+JonathanSeriesX@users.noreply.github.com>
2025-11-01 03:18:26 +0000

    It is now

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["next/core-web-vitals"],
-  ignorePatterns: ["content/**/*"],
-  rules: {
-    "@next/next/no-img-element": "off",
-  },
-};
