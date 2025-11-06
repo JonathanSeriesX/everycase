fc6df9c8d65ec4e69edd5b47abbacddb271fa887
JonathanSeriesX <12529409+JonathanSeriesX@users.noreply.github.com>
2025-11-06 22:43:28 +0400

    Fixed all the iPhones??

--- content/ancient/_meta.js
+++ content/ancient/_meta.js
@@ -1,15 +1,13 @@
 export default {
-  '*': {
+  "*": {
     theme: {
       pagination: false,
       toc: true,
       layout: "default",
     },
   },
-  'ipad-mini': 'iPad Mini 1–3',
-  'iphone-5c': 'iPhone 5c',
-  'ipad-2': 'iPad 2–4',
-  'iphone-4': 'iPhone 4 Bumpers',
-  'ipod-touch': 'iPod Touch Loop',
-  'ipod_socks': 'iPod Socks',
-}
+  "ipad-mini": "iPad Mini 1–3",
+  "ipad-2": "iPad 2–4",
+  "ipod-touch": "iPod Touch Loop",
+  "ipod-socks": "iPod Socks",
+};
