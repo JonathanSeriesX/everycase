60471b4a2b9321ca4d24549539df1acb28901b20
Evgenii Ostrovskii <12529409+JonathanSeriesX@users.noreply.github.com>
2026-06-28 23:49:13 +0100

    More minor data fixes

--- scripts/2_image_downloader.py
+++ scripts/2_image_downloader.py
@@ -19,7 +19,7 @@ BASE_URL_TEMPLATE = "https://store.storeimages.cdn-apple.com/8755/as-images.appl
 SCRIPT_DIR = Path(__file__).resolve().parent
 IMAGES_TO_DOWNLOAD_PATH = SCRIPT_DIR / "list.txt"
 SOURCE_IMAGES_PATH = SCRIPT_DIR / "source_images.txt"
-DEFAULT_TARGET_FOLDER = SCRIPT_DIR / "lower-res-redownload"
+DEFAULT_TARGET_FOLDER = Path("/Volumes/Storage/Images/lower-res-redownload")
 
 
 @dataclass(frozen=True)
